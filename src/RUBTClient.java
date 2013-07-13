df140991fb0453eb429c438a1d7e6299b4463f85
xtrusca <george.trusca@gmail.com>
2013-07-13 15:50:56 -0400

    files are now in place

---
+++ src/RUBTClient.java
@@ -0,0 +1,20 @@
+
+import java.io.IOException;
+
+
+
+import util.Data;
+
+
+public class RUBTClient {
+
+	public static void main(String[] args) throws IOException{
+		
+		String torrentFileName = "cs352.png.torrent";
+		String fileName = "finalName";
+		Data dataFile = new Data(torrentFileName, fileName);
+		
+	}
+}
+
+
